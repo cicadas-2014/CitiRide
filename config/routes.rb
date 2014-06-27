1f6e182d06c3d61e5f4463c5503f5d8bbb6723ef
jake <imasidonion@gmail.com>
2014-06-27 14:59:16 -0400

    adds users / rides resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   post '/' => 'searches#show', as: 'search'
 
+  resources :users, :rides
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
